2fa8cd28fc35db6cd3b601e09cd8aaaeb6f1e99f
Danrleygd <danrley.dias@estudante.ufjf.br>
2025-09-23 22:12:23 -0300

    Crai Componente "Disciplina"

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,11 +1,14 @@
-
+import './App.css'
+import Disciplina from './Disciplina'
 function App() {
 
   return (
 
     <>
+      <Disciplina />
       <h1>Olá Mundo!</h1>
       <h2>Olá React!</h2> 
+
     </>
 
   );
