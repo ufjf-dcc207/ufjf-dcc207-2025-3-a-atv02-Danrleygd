ab40a9bb72b3ad9b57691f559c38aac6a27ae69b
Danrleygd <danrley.dias@estudante.ufjf.br>
2025-09-25 22:11:37 -0300

    adiciona base

--- vite-project/src/Disciplina.tsx
+++ vite-project/src/Disciplina.tsx
@@ -5,12 +5,13 @@ type DisciplinaProps = {
   nome: string;
 };
 
-
 function Disciplina(props: DisciplinaProps) {
   return (
     <div className="disciplina">
+
       <h1>{props.codigo}</h1>
       <h2>{props.nome}</h2>
+      
     </div>
   );
 }
